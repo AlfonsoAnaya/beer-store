68f85c6740462c5980a64b8965bbb5ae3ee7ae03
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-05-11 18:01:51 +0200

    fetch punk api and prmitive display of data

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,12 +1,33 @@
+
+const urlBase = "https://api.punkapi.com/v2/beers?page=";
+let beers = []
+let page = 1
+
+//get Beers from Punk Api//
+async function getBeers() {
+    let url = urlBase + page
+    const beerPromise = await fetch(url);
+    beers = await beerPromise.json();
+    console.log(beers[0].tagline)
+    console.log(beers[0].tagline)
+}
+
+getBeers()
+
+const Card = ({beer: {name, tagline, description}}) => {
+    return(
+        <div>
+        <h2>{name}</h2>
+        <h3>{tagline}</h3>
+        <p>{description}</p>
+        </div>
+    )
+}
+
 function Beers() {
     return (
         <div className="beers">
-        <p >Card :3</p>
-        <p >Card :3</p>
-        <p >Card :3</p>
-        <p >Card :3</p>
-        <p >Card :3</p>
-        <p >Card :3</p>
+            {beers.map(beer => <Card beer={beer}/>)}
         </div>
     )
 }
