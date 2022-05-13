fd039695c32d99cac8dc6e7679a36390e0c05414
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-05-13 15:55:40 +0200

    further styles for cards to display info on hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,24 @@
 import React from "react"
 
-const Card = ({beer: {image_url, name, tagline, description}}) => {
+const Card = ({beer: {image_url, name, abv, ibu, tagline, description, food_pairing}}) => {
     return(
-        <div className="beer">
-        <img className="beer__img" src={image_url} alt={`a ${name} beer`}/>
-        <h2>{name}</h2>
-        <h3>{tagline}</h3>
-        <p>{description}</p>
+        <div className="beer-wrapper card">
+            <div className="beer">
+                <img className="beer__img" src={image_url} alt={`a ${name} beer`} />
+                <h3>{name}</h3>
+                <span className="beer__info">
+                    <span>ABV: {abv}%</span>
+                    <span>IBU: {ibu}</span>
+                </span>
+            </div>
+            <div className="beer__content">
+                <div className="beer__name">{name}</div>
+                <div className="beer__tagline">{tagline}</div>
+                <div className="beer__description">{description}</div>
+                <div className="beer_food-pairing">Pair with: {food_pairing.join(", ")}</div>
+            </div>
         </div>
+
     )
 }
 
